00cdeec211140b6b1df8936ef50517c3187183cd
Ed Scheinerman <ers@jhu.edu>
2016-07-25 08:15:21 -0400

    Help message for PowerSum

--- src/SimpleCombinatorics.jl
+++ src/SimpleCombinatorics.jl
@@ -371,9 +371,7 @@ end
 @doc """
 `PowerSum(n,k)` returns the sum of the `k`-th powers of the
 integers `1` through `n`, i.e.,
-```
-1^k + 2^k + 3^k + ... + n^k
-```
-"""
+`1^k + 2^k + 3^k + ... + n^k`.
+""" PowerSum
 
 end  #end of module
